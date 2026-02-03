03248d48145efccc6358d2481f9aafdf89f5accd
clark22134 <clark22134@gmail.com>
2026-02-02 17:16:37 -0800

    clarkfoster.com fully functional now!!!

--- terraform/modules/ecs/secrets.tf
+++ terraform/modules/ecs/secrets.tf
@@ -28,6 +28,57 @@ resource "aws_secretsmanager_secret" "contact_email" {
     Environment = var.environment
   }
 }
+
+# Admin Secrets
+resource "aws_secretsmanager_secret" "admin_username" {
+  name        = "portfolio/admin-username"
+  description = "Admin username for portfolio application"
+
+  tags = {
+    Name        = "portfolio-admin-username"
+    Environment = var.environment
+  }
+}
+
+resource "aws_secretsmanager_secret" "admin_password" {
+  name        = "portfolio/admin-password"
+  description = "Admin password for portfolio application"
+
+  tags = {
+    Name        = "portfolio-admin-password"
+    Environment = var.environment
+  }
+}
+
+resource "aws_secretsmanager_secret" "admin_email" {
+  name        = "portfolio/admin-email"
+  description = "Admin email for portfolio application"
+
+  tags = {
+    Name        = "portfolio-admin-email"
+    Environment = var.environment
+  }
+}
+
+resource "aws_secretsmanager_secret" "admin_fullname" {
+  name        = "portfolio/admin-fullname"
+  description = "Admin full name for portfolio application"
+
+  tags = {
+    Name        = "portfolio-admin-fullname"
+    Environment = var.environment
+  }
+}
+
+resource "aws_secretsmanager_secret" "jwt_secret" {
+  name        = "portfolio/jwt-secret"
+  description = "JWT secret key for portfolio application"
+
+  tags = {
+    Name        = "portfolio-jwt-secret"
+    Environment = var.environment
+  }
+}
 # Output the secret ARNs for use in task definition
 output "mail_username_arn" {
   value       = aws_secretsmanager_secret.mail_username.arn
@@ -42,4 +93,29 @@ output "mail_password_arn" {
 output "contact_email_arn" {
   value       = aws_secretsmanager_secret.contact_email.arn
   description = "ARN of contact email secret"
+}
+
+output "admin_username_arn" {
+  value       = aws_secretsmanager_secret.admin_username.arn
+  description = "ARN of admin username secret"
+}
+
+output "admin_password_arn" {
+  value       = aws_secretsmanager_secret.admin_password.arn
+  description = "ARN of admin password secret"
+}
+
+output "admin_email_arn" {
+  value       = aws_secretsmanager_secret.admin_email.arn
+  description = "ARN of admin email secret"
+}
+
+output "admin_fullname_arn" {
+  value       = aws_secretsmanager_secret.admin_fullname.arn
+  description = "ARN of admin full name secret"
+}
+
+output "jwt_secret_arn" {
+  value       = aws_secretsmanager_secret.jwt_secret.arn
+  description = "ARN of JWT secret"
 }
